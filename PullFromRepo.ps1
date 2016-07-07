bae71a286b5df46d7735583bc859ed8356811c2e
towianisci <towianisci@hotmail.com>
2016-07-07 12:50:50 -0700

    add push pull ps1 file

---
+++ PullFromRepo.ps1
@@ -0,0 +1,17 @@
+﻿[string]$localPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+Write-Output $localPath
+Set-Location -Path $localPath 
+$branch =git branch
+if(!("* master" -in $branch))
+{
+    git checkout master
+}
+git pull
+$status = git status
+if("Untracked files:" -in $status)
+{
+    Write-Output "Found Untracked files: Please run git commit -a -m and git push if want to update repository"
+    
+    Read-Host
+}
+Read-Host
