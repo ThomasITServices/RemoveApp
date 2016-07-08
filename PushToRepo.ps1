936936a316a3d649b4a66cdb7b1b9d736dbc7f39
towianisci <towianisci@hotmail.com>
2016-07-08 07:54:17 -0700

    update with try catch

--- PushToRepo.ps1
+++ PushToRepo.ps1
@@ -1,19 +1,28 @@
-﻿[string]$localPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-Write-Output $localPath
-Set-Location -Path $localPath 
-$branch =git branch
-if(!("* master" -in $branch))
+try
 {
-    git checkout master
-}
-git add .
-git commit -m "Updated code"
-git push
-$status = git status
-if("Untracked files:" -in $status)
-{
-    Write-Output "Found Untracked files: Please run git commit -a -m and git push if want to update repository"
+    Test-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "Git\cmd\git.exe" -Resolve -ErrorAction Stop)
+    [string]$localPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+    Write-Output $localPath
+    Set-Location -Path $localPath 
+    $branch =git branch
+    if(!("* master" -in $branch))
+    {
+        git checkout master
+    }
+    git add .
+    git commit -m "Updated code"
+    git push
+    $status = git status
+    if("Untracked files:" -in $status)
+    {
+        Write-Output "Found Untracked files: Please run git commit -a -m and git push if want to update repository"
     
+        Read-Host
+    }
     Read-Host
 }
-Read-Host
+catch
+{
+    Write-Warning -Message $_.Exception.Message 
+    Write-Output  -InputObject "Please make sure git is installed!"
+}
